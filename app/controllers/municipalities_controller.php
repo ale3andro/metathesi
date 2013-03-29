806ba97f2dd1e8f978ef35a7e3d0bc925b9ac7ad
ale3andro <ale3andro@gmail.com>
2013-03-29 20:40:16 +0200

    first working municipalities

--- app/controllers/municipalities_controller.php
+++ app/controllers/municipalities_controller.php
@@ -2,7 +2,24 @@
 	class MunicipalitiesController extends AppController
 	{
 		var $name="Municipalities";
+			
+		function getFromAAreaId($areaId)
+		{
+			$conditions['Municipality.level_1'] = 5;
+			$conditions['Municipality.a_area_id'] = $areaId;
+			
+			if (isset($this->params['requested']))
+				return $this->Municipality->find('all', array('conditions'=>$conditions));
+		}
 		
+		function getFromBAreaId($areaId)
+		{
+			$conditions['Municipality.level_1'] = 5;
+			$conditions['Municipality.b_area_id'] = $areaId;
+			
+			if (isset($this->params['requested']))
+				return $this->Municipality->find('all', array('conditions'=>$conditions));
+		}
 		
 	}
 	
