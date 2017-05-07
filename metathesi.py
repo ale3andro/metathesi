9604e8ebaaad34e4223ae1bde9a1e034fcc6ee61
ale3andro <ale3andro@gmail.com>
2017-05-07 22:39:09 +0300

    Προσθήκη πρώτων στατικών σελίδων

--- metathesi.py
+++ metathesi.py
@@ -34,34 +34,24 @@ def hello_world():
     for row in cur.fetchall():
         years_b.append([int(row[0]), row[0].decode('utf8')])
 
-    cur.execute('select * from provinces order by description')
-    provinces = []
-    for row in cur.fetchall():
-        provinces.append([row[0], row[1].decode('utf8')])
-
-    areas_a = []
-    areas_b = []
-    for item in provinces:
-        cur.execute('select * from a_areas where dipe_id=' + str(item[0]) + ' and id<1000 order by id')
-        for row in cur.fetchall():
-            if (row[2]==' '):
-                areas_a.append([row[0], row[4].decode('utf8')])
-            else:
-                areas_a.append([row[0], item[1] + " " + row[2].decode('utf8')])
-
-        cur.execute('select * from b_areas where dide_id=' + str(item[0]) + ' and id<1000 order by id')
-        for row in cur.fetchall():
-            if (row[2]==' '):
-                areas_b.append([row[0], row[4].decode('utf8')])
-            else:
-                areas_b.append([row[0], item[1] + " " + row[2].decode('utf8')])
-
     return render_template('homepage.html', a_eidikothtes=create_select_element("select", eidikothtes_a),
                                                 b_eidikothtes=create_select_element("select", eidikothtes_b),
                                                 a_years=create_select_element("select", years_a),
                                                 b_years=create_select_element("select", years_b),
-                                                a_areas=create_select_element("select", areas_a),
-                                                b_areas=create_select_element("select", areas_b))
+                                                a_areas=create_select_element("select", get_perioxes_a()),
+                                                b_areas=create_select_element("select", get_perioxes_b()))
+
+@app.route('/protobathmia')
+def show_protobathmia():
+    return render_template('oles_oi_perioxes_metathesis.html', areas=get_perioxes_a(), ba8mida='a')
+
+@app.route('/deuterobathmia')
+def show_deuterobathmia():
+    return render_template('oles_oi_perioxes_metathesis.html', areas=get_perioxes_b(), ba8mida='b')
+
+@app.route('/anoixtos_kodikas')
+def show_open_source():
+    return render_template('open_source.html');
 
 
 @app.route('/perioxes/<name>')
@@ -76,3 +66,34 @@ def create_select_element(element_name, values):
         retval += '<option value="' + str(item[0]) + '">' + item[1] + '</option>'
     retval += '</select>'
     return retval
+
+def get_provinces():
+    cur.execute('select * from provinces order by description')
+    provinces = []
+    for row in cur.fetchall():
+        provinces.append([row[0], row[1].decode('utf8')])
+    return provinces
+
+def get_perioxes_a():
+    provinces = get_provinces()
+    areas_a = []
+    for item in provinces:
+        cur.execute('select * from a_areas where dipe_id=' + str(item[0]) + ' and id<1000 order by id')
+        for row in cur.fetchall():
+            if (row[2]==' '):
+                areas_a.append([row[0], row[4].decode('utf8')])
+            else:
+                areas_a.append([row[0], item[1] + " " + row[2].decode('utf8')])
+    return areas_a
+
+def get_perioxes_b():
+    provinces = get_provinces()
+    areas_b = []
+    for item in provinces:
+        cur.execute('select * from b_areas where dide_id=' + str(item[0]) + ' and id<1000 order by id')
+        for row in cur.fetchall():
+            if (row[2]==' '):
+                areas_b.append([row[0], row[4].decode('utf8')])
+            else:
+                areas_b.append([row[0], item[1] + " " + row[2].decode('utf8')])
+    return areas_b
